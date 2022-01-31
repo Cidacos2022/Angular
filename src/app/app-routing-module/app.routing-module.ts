0c0cd52055322e3061e54d281c7560362089fb8e
rafael alamanac <you@example.com>
2022-01-31 18:39:33 +0100

    Add default routeg

--- src/app/app-routing-module/app.routing-module.ts
+++ src/app/app-routing-module/app.routing-module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router'; 
+import { Routes, RouterModule } from '@angular/router';
 import { HelloWorldInputParComponent } from '../hello-world-input-par/hello-world-input-par.component';
+import { HelloWorldPipesComponent } from '../hello-world-pipes/hello-world-pipes.component';
 import { HelloWorldComponent } from '../hello-world/hello-world.component';
 
 const routes: Routes = [
-    { path: 'hello-world', component: HelloWorldComponent },
-    {
-      path: 'input',
-      component: HelloWorldInputParComponent,
-    },
-  ];  
+  {
+    path: '',
+    component: HelloWorldPipesComponent,
+  },
+  { path: 'hello-world', component: HelloWorldComponent },
+  {
+    path: 'input',
+    component: HelloWorldInputParComponent,
+  },
+];
 
 // configures NgModule imports and exports
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
